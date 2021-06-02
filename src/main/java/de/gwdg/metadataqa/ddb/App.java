13f179a52f9893c96d4fdc4af2aaba254b671785
pkiraly <pkiraly@gwdg.de>
2021-06-02 09:33:31 +0200

    Turning into a Maven project

---
+++ src/main/java/de/gwdg/metadataqa/ddb/App.java
@@ -0,0 +1,11 @@
+package de.gwdg.metadataqa.ddb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
