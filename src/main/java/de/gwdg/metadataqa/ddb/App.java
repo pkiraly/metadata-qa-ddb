6b7975fd3f6fb64ed638873c927ce945eef5bdaf
pkiraly <pkiraly@gwdg.de>
2021-11-08 13:49:32 +0100

    Update application.

--- src/main/java/de/gwdg/metadataqa/ddb/App.java
+++ src/main/java/de/gwdg/metadataqa/ddb/App.java
@@ -19,13 +19,18 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  */
 public class App {
     private static final Logger logger = Logger.getLogger(App.class.getCanonicalName());
+    public enum FORMAT {CSV, JSON};
 
     public static void main(String[] args) throws IOException, ParseException {
         Options options = new Options();
@@ -33,20 +38,33 @@ public class App {
         options.addOption(new Option("s", "schema", true, "schema configuration file"));
         options.addOption(new Option("i", "input", true, "input file"));
         options.addOption(new Option("o", "output", true, "output file"));
+        options.addOption(new Option("f", "format", true, "output format"));
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse( options, args);
         String inputFile = cmd.getOptionValue("input");
         String schemaFile = cmd.getOptionValue("schema");
         String outputFile = cmd.getOptionValue("output");
+        FORMAT format = cmd.getOptionValue("format").toLowerCase(Locale.ROOT).equals("csv") ? FORMAT.CSV : FORMAT.JSON;
         CalculatorFacade calculator = initializeCalculator(schemaFile);
 
         try {
             XPathBasedIterator iterator = new XPathBasedIterator(new File(inputFile), "//record");
-            String csv = null;
+            String line = null;
             try (var writer = Files.newBufferedWriter(Paths.get(outputFile))) {
+                if (format.equals(FORMAT.CSV)) {
+                    List<String> headers = new ArrayList<>();
+                    for (String h : calculator.getHeader()) {
+                        System.err.println(h);
+                    }
+                    line = StringUtils.join(calculator.getHeader(), ",");
+                    writer.write(line + "\n");
+                }
                 while (iterator.hasNext()) {
-                    csv = calculator.measureAsJson(iterator.next());
-                    writer.write(csv + "\n");
+                    if (format.equals(FORMAT.CSV))
+                        line = calculator.measure(iterator.next());
+                    else
+                        line = calculator.measureAsJson(iterator.next());
+                    writer.write(line + "\n");
                 }
             } catch (IOException e) {
                 logger.log(Level.SEVERE, "Some I/O issue happened", e);
@@ -69,7 +87,9 @@ public class App {
           .disableFieldExistenceMeasurement()
           .disableFieldCardinalityMeasurement()
           .enableRuleCatalogMeasurement()
-          .disableFieldExtractor();
+          .enableFieldExtractor()
+          .withOnlyIdInHeader(true)
+        ;
 
         CalculatorFacade calculator = new CalculatorFacade(configuration)
           .setSchema(schema);
