9a862ef74d1fd60e11ece9e539bffdd9f2fca349
pkiraly <pkiraly@gwdg.de>
2021-11-29 13:41:01 +0100

    CSV: only status values

--- src/main/java/de/gwdg/metadataqa/ddb/App.java
+++ src/main/java/de/gwdg/metadataqa/ddb/App.java
@@ -3,6 +3,8 @@ package de.gwdg.metadataqa.ddb;
 import de.gwdg.metadataqa.api.calculator.CalculatorFacade;
 import de.gwdg.metadataqa.api.configuration.ConfigurationReader;
 import de.gwdg.metadataqa.api.configuration.MeasurementConfiguration;
+import de.gwdg.metadataqa.api.interfaces.Calculator;
+import de.gwdg.metadataqa.api.rule.RuleCheckingOutputType;
 import de.gwdg.metadataqa.api.schema.Schema;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -39,13 +41,19 @@ public class App {
         options.addOption(new Option("i", "input", true, "input file"));
         options.addOption(new Option("o", "output", true, "output file"));
         options.addOption(new Option("f", "format", true, "output format"));
+        options.addOption(new Option("x", "indexing", false, "do indexing"));
+        options.addOption(new Option("p", "path", true, "Solr path"));
         CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = parser.parse( options, args);
+        CommandLine cmd = parser.parse(options, args);
+        logger.info("cmd: " + cmd.toString());
+
         String inputFile = cmd.getOptionValue("input");
         String schemaFile = cmd.getOptionValue("schema");
         String outputFile = cmd.getOptionValue("output");
+        boolean indexing = cmd.hasOption("indexing");
+        String solrPath = cmd.hasOption("path") ? cmd.getOptionValue("path") : null;
         FORMAT format = cmd.getOptionValue("format").toLowerCase(Locale.ROOT).equals("csv") ? FORMAT.CSV : FORMAT.JSON;
-        CalculatorFacade calculator = initializeCalculator(schemaFile);
+        CalculatorFacade calculator = initializeCalculator(schemaFile, indexing, solrPath);
 
         try {
             XPathBasedIterator iterator = new XPathBasedIterator(new File(inputFile), "//record");
@@ -66,6 +74,7 @@ public class App {
                         line = calculator.measureAsJson(iterator.next());
                     writer.write(line + "\n");
                 }
+                calculator.shutDown();
             } catch (IOException e) {
                 logger.log(Level.SEVERE, "Some I/O issue happened", e);
             }
@@ -79,17 +88,35 @@ public class App {
         }
     }
 
-    private static CalculatorFacade initializeCalculator(String schemaFile) throws FileNotFoundException {
+    private static CalculatorFacade initializeCalculator(String schemaFile, boolean indexing, String solrPath) throws FileNotFoundException {
+        logger.info("indexing: " + indexing);
+        logger.info("solrPath: " + solrPath);
         Schema schema = ConfigurationReader.readSchemaYaml(schemaFile).asSchema();
+        logger.info("RecordId: " + schema.getRecordId());
 
-        MeasurementConfiguration configuration = new MeasurementConfiguration()
-          .disableCompletenessMeasurement()
-          .disableFieldExistenceMeasurement()
-          .disableFieldCardinalityMeasurement()
-          .enableRuleCatalogMeasurement()
-          .enableFieldExtractor()
-          .withOnlyIdInHeader(true)
-        ;
+        MeasurementConfiguration configuration = null;
+        if (indexing) {
+            configuration = new MeasurementConfiguration()
+              .disableCompletenessMeasurement()
+              .disableFieldExistenceMeasurement()
+              .disableFieldCardinalityMeasurement()
+              .disableRuleCatalogMeasurement()
+              .disableUniquenessMeasurement()
+              .disableFieldExtractor()
+              .withSolrConfiguration("localhost", "8983", solrPath)
+              .enableIndexing()
+            ;
+        } else {
+            configuration = new MeasurementConfiguration()
+              .disableCompletenessMeasurement()
+              .disableFieldExistenceMeasurement()
+              .disableFieldCardinalityMeasurement()
+              .enableRuleCatalogMeasurement()
+              .enableFieldExtractor()
+              .withOnlyIdInHeader(true)
+              .withRuleCheckingOutputType(RuleCheckingOutputType.STATUS)
+            ;
+        }
 
         CalculatorFacade calculator = new CalculatorFacade(configuration)
           .setSchema(schema);
