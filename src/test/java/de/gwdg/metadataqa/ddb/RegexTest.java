b526b62b0029db61abbe72c4bafff81b1918d78b
pkiraly <pkiraly@gwdg.de>
2022-06-23 22:56:10 +0200

    Update MARC schema.

--- src/test/java/de/gwdg/metadataqa/ddb/RegexTest.java
+++ src/test/java/de/gwdg/metadataqa/ddb/RegexTest.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static org.junit.Assert.assertTrue;
+
 public class RegexTest {
   @Test
   public void name() {
     String url = "http://mdz-nbn-resolving.de/urn:nbn:de:bvb:12-bsb10020668-4";
     Matcher m = Pattern.compile("^[a-zA-Z_0-9:/\\.\\-]+$").matcher(url);
-    if (m.matches()) {
-      System.err.println("matches");
-    }
+    assertTrue(m.matches());
   }
 
 }
