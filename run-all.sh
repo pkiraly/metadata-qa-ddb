02cb21dbc833f8f7808394425b085687ec776a33
Péter Király <pkiraly@gwdg.de>
2024-12-05 13:55:07 +0100

    Dockerize application

---
+++ run-all.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+ROOT=$(realpath $(dirname $0))
+source $ROOT/configuration.cnf
+echo "OUTPUT_DIR: $OUTPUT_DIR"
+
+rm $OUTPUT_DIR/*
+
+# create empty tables
+scripts/create_database.mysql.sh
+# creates files.csv
+scripts/ingest/03_extract_basic_info_from_downloaded_files.sh
+# import into files table
+scripts/ingest/04_import_basic_info.mysql.sh
+
+# populate file_record in MySQL,
+# create and populate record in SQLite3
+# index record with Solr
+scripts/index/03_index_ddb-dc.sh
+# create issue.csv
+scripts/process/03_process_ddb-dc.sh
+# import into issue table
+scripts/process/11_import_dc.mysql.sh
+
+# populate file_record, and record in SQLite3
+scripts/index/04_index_lido.sh
+# create issue.csv
+scripts/process/04_process_lido.sh
+# import into issue table
+scripts/process/11_import_lido.mysql.sh
+
+scripts/process/12_calculate_aggregations.mysql.sh
