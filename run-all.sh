f0525f168b9dd5801858d06f8589b0d42ecfb97f
Péter Király <pkiraly@gwdg.de>
2024-12-11 09:18:20 +0100

    Add dependencies to schemas

--- run-all.sh
+++ run-all.sh
@@ -4,6 +4,9 @@ ROOT=$(realpath $(dirname $0))
 source $ROOT/configuration.cnf
 echo "OUTPUT_DIR: $OUTPUT_DIR"
 
+PROCESS_DC=1
+PROCESS_LIDO=1
+
 rm $OUTPUT_DIR/*
 
 # create empty tables
@@ -13,20 +16,24 @@ scripts/ingest/03_extract_basic_info_from_downloaded_files.sh
 # import into files table
 scripts/ingest/04_import_basic_info.mysql.sh
 
-# populate file_record in MySQL,
-# create and populate record in SQLite3
-# index record with Solr
-scripts/index/03_index_ddb-dc.sh
-# create issue.csv
-scripts/process/03_process_ddb-dc.sh
-# import into issue table
-scripts/process/11_import_dc.mysql.sh
+if [[ "${PROCESS_DC}" == "1" ]]; then
+  # populate file_record in MySQL,
+  # create and populate record in SQLite3
+  # index record with Solr
+  scripts/index/03_index_ddb-dc.sh
+  # create issue.csv
+  scripts/process/03_process_ddb-dc.sh
+  # import into issue table
+  scripts/process/11_import_dc.mysql.sh
+fi
 
-# populate file_record, and record in SQLite3
-scripts/index/04_index_lido.sh
-# create issue.csv
-scripts/process/04_process_lido.sh
-# import into issue table
-scripts/process/11_import_lido.mysql.sh
+if [[ "${PROCESS_LIDO}" == "1" ]]; then
+  # populate file_record, and record in SQLite3
+  scripts/index/04_index_lido.sh
+  # create issue.csv
+  scripts/process/04_process_lido.sh
+  # import into issue table
+  scripts/process/11_import_lido.mysql.sh
+fi
 
 scripts/process/12_calculate_aggregations.mysql.sh
