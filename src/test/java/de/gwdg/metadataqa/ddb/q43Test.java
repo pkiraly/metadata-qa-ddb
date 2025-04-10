78d6c7518e9a8368d53ef71e5389f37eb3aa5c8d
Péter Király <pkiraly@gwdg.de>
2025-04-10 12:43:21 +0200

    Update 5.1

---
+++ src/test/java/de/gwdg/metadataqa/ddb/q43Test.java
@@ -0,0 +1,66 @@
+package de.gwdg.metadataqa.ddb;
+
+import de.gwdg.metadataqa.api.configuration.ConfigurationReader;
+import de.gwdg.metadataqa.api.counter.FieldCounter;
+import de.gwdg.metadataqa.api.model.selector.Selector;
+import de.gwdg.metadataqa.api.model.selector.SelectorFactory;
+import de.gwdg.metadataqa.api.rule.RuleChecker;
+import de.gwdg.metadataqa.api.rule.RuleCheckerOutput;
+import de.gwdg.metadataqa.api.rule.RuleCheckingOutputStatus;
+import de.gwdg.metadataqa.api.rule.RuleCheckingOutputType;
+import de.gwdg.metadataqa.api.rule.singlefieldchecker.HasChildrenChecker;
+import de.gwdg.metadataqa.api.schema.Schema;
+import de.gwdg.metadataqa.api.xml.XPathWrapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+public class q43Test {
+  private Schema schema;
+  private String recordAddress = "//lido:lido";
+  private XPathWrapper xPathWrapper;
+  private String xml;
+
+  @Before
+  public void setUp() throws Exception {
+    URL url = this.getClass().getResource("/lido/test-Q-9.4.xml");
+    File file = new File(url.getFile());
+    assertTrue(file.exists());
+
+    try {
+      schema = ConfigurationReader.readSchemaYaml("schemas/lido-schema.yaml").asSchema();
+      XPathBasedIterator iterator = new XPathBasedIterator(file, recordAddress, schema.getNamespaces());
+      xml = iterator.next();
+      xPathWrapper = new XPathWrapper(xml, schema.getNamespaces());
+    } catch (XPathExpressionException | IOException | ParserConfigurationException | SAXException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void name() {
+    Selector cache = SelectorFactory.getInstance(schema.getFormat(), xml);
+    FieldCounter<RuleCheckerOutput> fieldCounter = new FieldCounter<>();
+    List<String> ids = List.of("Q-3.x", "Q-4.x", "Q-4.3");
+    for (RuleChecker checker : schema.getRuleCheckers()) {
+      if (ids.contains(checker.getId()))
+        checker.update(cache, fieldCounter, RuleCheckingOutputType.STATUS);
+    }
+    System.err.println(fieldCounter);
+    assertEquals(
+      RuleCheckingOutputStatus.PASSED,
+      fieldCounter.get("Q-4.3").getStatus()
+    );
+
+  }
+}
