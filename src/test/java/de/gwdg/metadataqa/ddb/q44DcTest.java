711a7e3748af5d6f7eefd7260051b74e6f6c32c3
Péter Király <pkiraly@gwdg.de>
2025-04-23 15:46:37 +0200

    Fixing 4.4

---
+++ src/test/java/de/gwdg/metadataqa/ddb/q44DcTest.java
@@ -0,0 +1,104 @@
+package de.gwdg.metadataqa.ddb;
+
+import de.gwdg.metadataqa.api.configuration.ConfigurationReader;
+import de.gwdg.metadataqa.api.counter.FieldCounter;
+import de.gwdg.metadataqa.api.model.selector.Selector;
+import de.gwdg.metadataqa.api.model.selector.SelectorFactory;
+import de.gwdg.metadataqa.api.rule.RuleChecker;
+import de.gwdg.metadataqa.api.rule.RuleCheckerOutput;
+import de.gwdg.metadataqa.api.rule.RuleCheckingOutputStatus;
+import de.gwdg.metadataqa.api.rule.RuleCheckingOutputType;
+import de.gwdg.metadataqa.api.schema.Schema;
+import de.gwdg.metadataqa.api.xml.XPathWrapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+public class q44DcTest {
+  private Schema schema;
+  private String recordAddress = "//oai:record | //rdf:Description";
+  private XPathWrapper xPathWrapper;
+  private String xml;
+
+  public void setup(String fileName) throws Exception {
+    URL url = this.getClass().getResource("/dc/" + fileName);
+    File file = new File(url.getFile());
+    assertTrue(file.exists());
+
+    try {
+      schema = ConfigurationReader.readSchemaYaml("schemas/rdf-dc-schema.yaml").asSchema();
+      XPathBasedIterator iterator = new XPathBasedIterator(file, recordAddress, schema.getNamespaces());
+      xml = iterator.next();
+      xPathWrapper = new XPathWrapper(xml, schema.getNamespaces());
+    } catch (XPathExpressionException | IOException | ParserConfigurationException | SAXException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void passed() throws Exception {
+    setup("Q-4.4.xml");
+    Selector cache = SelectorFactory.getInstance(schema.getFormat(), xml);
+    FieldCounter<RuleCheckerOutput> fieldCounter = new FieldCounter<>();
+    List<String> ids = List.of("Q-4.4.0", "Q-4.4");
+    for (RuleChecker checker : schema.getRuleCheckers()) {
+      if (ids.contains(checker.getId())) {
+        checker.setDebug();
+        checker.update(cache, fieldCounter, RuleCheckingOutputType.STATUS);
+      }
+    }
+    // System.err.println(fieldCounter);
+    assertEquals(
+      RuleCheckingOutputStatus.PASSED,
+      fieldCounter.get("Q-4.4").getStatus()
+    );
+  }
+
+  @Test
+  public void emptyTag() throws Exception {
+    setup("Q-4.4-leer.xml");
+    Selector cache = SelectorFactory.getInstance(schema.getFormat(), xml);
+    FieldCounter<RuleCheckerOutput> fieldCounter = new FieldCounter<>();
+    List<String> ids = List.of("Q-4.4.0", "Q-4.4");
+    for (RuleChecker checker : schema.getRuleCheckers()) {
+      if (ids.contains(checker.getId())) {
+        checker.setDebug();
+        checker.update(cache, fieldCounter, RuleCheckingOutputType.STATUS);
+      }
+    }
+    System.err.println(fieldCounter);
+    assertEquals(
+      RuleCheckingOutputStatus.FAILED,
+      fieldCounter.get("Q-4.4").getStatus()
+    );
+  }
+
+  @Test
+  public void missingTag() throws Exception {
+    setup("Q-4.4-missing.xml");
+    Selector cache = SelectorFactory.getInstance(schema.getFormat(), xml);
+    FieldCounter<RuleCheckerOutput> fieldCounter = new FieldCounter<>();
+    List<String> ids = List.of("Q-4.4.0", "Q-4.4");
+    for (RuleChecker checker : schema.getRuleCheckers()) {
+      if (ids.contains(checker.getId())) {
+        checker.setDebug();
+        checker.update(cache, fieldCounter, RuleCheckingOutputType.STATUS);
+      }
+    }
+    System.err.println(fieldCounter);
+    assertEquals(
+      RuleCheckingOutputStatus.NA,
+      fieldCounter.get("Q-4.4").getStatus()
+    );
+  }
+}
