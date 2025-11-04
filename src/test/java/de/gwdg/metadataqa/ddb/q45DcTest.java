dc9f477f6977861f97f48ddb8cdaded123869100
Péter Király <pkiraly@gwdg.de>
2025-11-04 22:12:16 +0100

    Fixing 4.5 and 4.6

--- src/test/java/de/gwdg/metadataqa/ddb/q45DcTest.java
+++ src/test/java/de/gwdg/metadataqa/ddb/q45DcTest.java
@@ -13,6 +13,25 @@ import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
 
+/**
+ * rdf:Description/edm:isShownAt/edm:WebResource/@rdf:about
+ * rdf:Description/edm:isShownAt/@rdf:resource
+ * oai:record/oai:metadata/oai_dc:dc/edm:isShownAt
+ *
+ * Check whether the data record contains an object in context (see table "Mapping the DDB elements").
+ * If so => check whether this element contain a value.
+ *
+ * and
+ *  - dependencies: [ Q-4.5or ]
+ *  - minCount: 1
+ *  - minLength: 1
+ *
+ * Q-4.5or
+ * or:
+ *  - dependencies: [ Q-4.5b ]
+ *  - dependencies: [ Q-4.5c ]
+ *  - dependencies: [ Q-4.5d ]
+ */
 public class q45DcTest extends DcTest {
   @Test
   public void name() throws Exception {
@@ -29,5 +48,41 @@ public class q45DcTest extends DcTest {
     assertEquals(
         RuleCheckingOutputStatus.FAILED,
         fieldCounter.get("Q-4.5").getStatus());
+  }
+
+  @Test
+  public void edmIsShownAt_rdfResource() throws Exception {
+    setup("Q-4.5-edmIsShownAt_rdfResource.xml");
+    Selector cache = SelectorFactory.getInstance(schema.getFormat(), xml);
+    FieldCounter<RuleCheckerOutput> fieldCounter = new FieldCounter<>();
+    List<String> ids = List.of("Q-4.5a", "Q-4.5b", "Q-4.5c", "Q-4.5d", "Q-4.5or", "Q-4.5");
+    for (RuleChecker checker : schema.getRuleCheckers()) {
+      if (ids.contains(checker.getId())) {
+        checker.setDebug();
+        checker.update(cache, fieldCounter, RuleCheckingOutputType.STATUS);
+      }
+    }
+    System.err.println(fieldCounter);
+    assertEquals(
+      RuleCheckingOutputStatus.PASSED,
+      fieldCounter.get("Q-4.5").getStatus());
+  }
+
+  @Test
+  public void edmIsShownAt_Broken_Links() throws Exception {
+    setup("Q-4.5-edmIsShownAt_Broken_Links.xml");
+    Selector cache = SelectorFactory.getInstance(schema.getFormat(), xml);
+    FieldCounter<RuleCheckerOutput> fieldCounter = new FieldCounter<>();
+    List<String> ids = List.of("Q-4.5a", "Q-4.5b", "Q-4.5c", "Q-4.5d", "Q-4.5or", "Q-4.5");
+    for (RuleChecker checker : schema.getRuleCheckers()) {
+      if (ids.contains(checker.getId())) {
+        checker.setDebug();
+        checker.update(cache, fieldCounter, RuleCheckingOutputType.STATUS);
+      }
+    }
+    System.err.println(fieldCounter);
+    assertEquals(
+      RuleCheckingOutputStatus.PASSED,
+      fieldCounter.get("Q-4.5").getStatus());
   }
 }
